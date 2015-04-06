beaacdbcf9f3d1fdd01735dccfbdd5f18f964a4b
freepk <freepk@mail.ru>
2015-04-06 11:36:47 +0000

    .

--- hashtab.go
+++ hashtab.go
@@ -1,85 +1,72 @@
 package hashtab
 
 import (
-        "sync/atomic"
-        "unsafe"
-)
-
-const (
-        entrySize     = 8
-        HashTabMaxLen = 65536
+	"sync/atomic"
 )
 
 type entry struct {
-        key   uint32
-        value uint32
+	key   uint32
+	value uint32
 }
 
 type HashTab struct {
-        len     uint32
-        entries []entry
+	mask    uint32
+	entries []entry
 }
 
 func hash(v uint32) uint32 {
-        v ^= v >> 16
-        v *= 0x85ebca6b
-        v ^= v >> 13
-        v *= 0xc2b2ae35
-        v ^= v >> 16
-        return v
+	v ^= v >> 16
+	v *= 0x85ebca6b
+	v ^= v >> 13
+	v *= 0xc2b2ae35
+	v ^= v >> 16
+	return v
 }
 
-func NewHashTab(len uint32) *HashTab {
-        var buf []byte
-        var ptr unsafe.Pointer
-
-        buf = make([]byte, len*entrySize)
-        ptr = unsafe.Pointer(&buf[0])
-        return &HashTab{len: len, entries: (*[HashTabMaxLen]entry)(ptr)[:len]}
-}
-
-func (h *HashTab) Len() uint32 {
-	return h.len
+func NewHashTab(power uint32) *HashTab {
+	return &HashTab{
+		mask:    (1 << power) - 1,
+		entries: make([]entry, (1 << power))}
 }
 
 func (h *HashTab) Set(k, v uint32) {
-        var i, t uint32
-        var e *entry
+	var i, t uint32
+	var e *entry
 
-        i = hash(k)
-        for {
-                i &= (h.len - 1)
-                e = &h.entries[i]
-                t = atomic.LoadUint32(&e.key)
-                if t == k {
-                        atomic.StoreUint32(&e.value, v)
-                        return
-                }
-                if t == 0 {
-                        if atomic.CompareAndSwapUint32(&e.key, 0, k) {
-                                atomic.StoreUint32(&e.value, v)
-                                return
-                        }
-                }
-                i++
-        }
+	i = hash(k)
+	for {
+		i &= h.mask
+		e = &h.entries[i]
+		t = atomic.LoadUint32(&e.key)
+		if t == k {
+			atomic.StoreUint32(&e.value, v)
+			return
+		}
+		if t == 0 {
+			if atomic.CompareAndSwapUint32(&e.key, 0, k) {
+				atomic.StoreUint32(&e.value, v)
+				return
+			}
+		}
+		i++
+	}
 }
 
 func (h *HashTab) Get(k uint32) uint32 {
-        var i, t uint32
-        var e *entry
+	var i, t uint32
+	var e *entry
 
-        i = hash(k)
-        for {
-                i &= (h.len - 1)
-                e = &h.entries[i]
-                t = atomic.LoadUint32(&e.key)
-                if t == k {
-                        return atomic.LoadUint32(&e.value)
-                }
-                if t == 0 {
-                        return 0
-                }
-                i++
-        }
+	i = hash(k)
+	for {
+		i &= h.mask
+		e = &h.entries[i]
+		t = atomic.LoadUint32(&e.key)
+		if t == k {
+			return atomic.LoadUint32(&e.value)
+		}
+		if t == 0 {
+			return 0
+		}
+		i++
+	}
 }
--- hashtab_test.go
+++ hashtab_test.go
@@ -1,20 +1,20 @@
 package hashtab
 
 import (
-        "testing"
+	"testing"
 )
 
 func TestHashTab(t *testing.T) {
-        h := NewHashTab(64)
-        h.Set(100, 1000)
-        h.Set(101, 1001)
-        if h.Get(100) != 1000 {
-                t.Fail()
-        }
-        if h.Get(101) != 1001 {
-                t.Fail()
-        }
-        if h.Get(50) != 0 {
-                t.Fail()
-        }
+	h := NewHashTab(10)
+	h.Set(100, 1000)
+	h.Set(101, 1001)
+	if h.Get(100) != 1000 {
+		t.Fail()
+	}
+	if h.Get(101) != 1001 {
+		t.Fail()
+	}
+	if h.Get(50) != 0 {
+		t.Fail()
+	}
 }
