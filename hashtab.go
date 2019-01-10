d79edae9e1d930752086d91ed92ee7e85a90a967
freepk <freepk@mail.ru>
2019-01-10 23:42:52 +0000

    Refactor

--- hashtab.go
+++ hashtab.go
@@ -1,50 +1,52 @@
 package hashtab
 
 import (
+	"errors"
 	"sync/atomic"
 )
 
 type entry struct {
-	key   uint32
-	value uint32
+	key   uint64
+	value uint64
 }
 
 type HashTab struct {
-	mask    uint32
+	mask    uint64
 	entries []entry
 }
 
-func hash(v uint32) uint32 {
-	v ^= v >> 16
-	v *= 0x85ebca6b
-	v ^= v >> 13
-	v *= 0xc2b2ae35
-	v ^= v >> 16
-	return v
+const MaxPower = 32
+
+var (
+	ExceedMaxPowerError = errors.New("Exceeded max power.")
+)
+
+func NewHashTab(power uint8) (*HashTab, error) {
+	if power > MaxPower {
+		return nil, ExceedMaxPowerError
+	}
+	size := uint64(1 << power)
+	mask := size - 1
+	entries := make([]entry, size)
+	return &HashTab{mask: mask, entries: entries}, nil
 }
 
-func NewHashTab(power uint32) *HashTab {
-	return &HashTab{
-		mask:    (1 << power) - 1,
-		entries: make([]entry, (1 << power))}
-}
-
-func (h *HashTab) Set(k, v uint32) {
-	var i, t uint32
+func (h *HashTab) Set(k, v uint64) {
+	var i, t uint64
 	var e *entry
 
-	i = hash(k)
+	i = k
 	for {
 		i &= h.mask
 		e = &h.entries[i]
-		t = atomic.LoadUint32(&e.key)
+		t = atomic.LoadUint64(&e.key)
 		if t == k {
-			atomic.StoreUint32(&e.value, v)
+			atomic.StoreUint64(&e.value, v)
 			return
 		}
 		if t == 0 {
-			if atomic.CompareAndSwapUint32(&e.key, 0, k) {
-				atomic.StoreUint32(&e.value, v)
+			if atomic.CompareAndSwapUint64(&e.key, 0, k) {
+				atomic.StoreUint64(&e.value, v)
 				return
 			}
 		}
@@ -52,17 +54,17 @@ func (h *HashTab) Set(k, v uint32) {
 	}
 }
 
-func (h *HashTab) Get(k uint32) uint32 {
-	var i, t uint32
+func (h *HashTab) Get(k uint64) uint64 {
+	var i, t uint64
 	var e *entry
 
-	i = hash(k)
+	i = k
 	for {
 		i &= h.mask
 		e = &h.entries[i]
-		t = atomic.LoadUint32(&e.key)
+		t = atomic.LoadUint64(&e.key)
 		if t == k {
-			return atomic.LoadUint32(&e.value)
+			return atomic.LoadUint64(&e.value)
 		}
 		if t == 0 {
 			return 0
--- hashtab_test.go
+++ hashtab_test.go
@@ -5,7 +5,10 @@ import (
 )
 
 func TestHashTab(t *testing.T) {
-	h := NewHashTab(10)
+	h, err := NewHashTab(10)
+	if err != nil {
+		t.Fail()
+	}
 	h.Set(100, 1000)
 	h.Set(101, 1001)
 	if h.Get(100) != 1000 {
